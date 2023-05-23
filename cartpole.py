e4d08590768e67c904d76f55211b4019756b9e59
Lachlan <112550234+lchlnprc@users.noreply.github.com>
2023-05-23 18:06:00 +1200

    Added Graph and Average

--- cartpole.py
+++ cartpole.py
@@ -1,6 +1,8 @@
 import random
 import gym
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten
@@ -10,21 +12,17 @@ from rl.agents import DQNAgent
 from rl.policy import BoltzmannQPolicy
 from rl.memory import SequentialMemory
 
-##CARTPOLE WORKING
-
 env = gym.make("CartPole-v1")
 
-states = env.observation_space.shape[0] #amount of states
-actions = env.action_space.n  #amount of actions
+states = env.observation_space.shape[0] 
+actions = env.action_space.n  
 
-model = Sequential()        #create a machine learning model and neural network model
+model = Sequential() 
 model.add(Flatten(input_shape=(1, states)))
 model.add(Dense(24, activation="relu"))
 model.add(Dense(24, activation="relu"))
 model.add(Dense(actions, activation="linear"))
 
-
-#Create a Deep Q Learning Agent with Sequential Memory and the Boltzmann Q Policy
 agent = DQNAgent(
     model=model, 
     memory=SequentialMemory(limit=50000, window_length=1),
@@ -34,16 +32,33 @@ agent = DQNAgent(
     target_model_update=0.01
 )
 
-
 #Compile the model with the Adam optimizer
 agent.compile(Adam(lr=0.001), metrics=["mae"])
-agent.fit(env, nb_steps=10000, visualize=False, verbose=1)
+history = agent.fit(env, nb_steps=30000, visualize=False, verbose=1)
+
+# Saving the rewards in a list
+reward_history = history.history["episode_reward"]
 
 #Evaluate agent
 results = agent.test(env, nb_episodes=10, visualize=True)
-print(np.mean(results.history["episode_reward"]))
+
+# Print the average reward for the evaluation episodes
+avg_reward = np.mean(results.history["episode_reward"])
+print(f'Average reward: {avg_reward}')
+
+# Plotting rewards
+plt.figure(figsize=(12, 8))
+plt.plot(reward_history, label='Rewards')
+slope, intercept, r_value, p_value, std_err = stats.linregress(range(len(reward_history)),reward_history)
+plt.plot([0,len(reward_history)],[intercept,intercept+slope*len(reward_history)], 'r', label='Trendline') # Plotting trendline
+plt.xlabel('Episodes')
+plt.ylabel('Rewards')
+plt.title('Rewards across training episodes')
+plt.legend()
+plt.show()
 
 env.close()
+
 
 
 
